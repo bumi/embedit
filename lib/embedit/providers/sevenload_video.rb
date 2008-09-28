0bba6c27b027277a9c0ee3090e69f9e86fbbb006
Michael Bumann <michael@railslove.com>
2008-09-28 21:46:26 +0200

    added specs for sevenload video

--- lib/embedit/providers/sevenload_video.rb
+++ lib/embedit/providers/sevenload_video.rb
@@ -2,7 +2,7 @@ module Embedit
 
   class SevenloadVideo
   
-    attr_reader :title, :url, :format
+    attr_reader :title, :url, :format, :video_id, :lang
   
     def initialize(url)
       @format = 'video'
@@ -11,28 +11,35 @@ module Embedit
     end
     
     def html(options = {})
-      options.reverse_merge({:width=>425,:height=>350})
+      options.reverse_merge!({:width=>445,:height=>364})
       %{
-        <object width="425" height="350">
-          <param name="wmode" value="transparent"></param>
-          <param name="FlashVars" value="slxml=%ssevenload.com"/>
-          <param name="movie" value="http://%ssevenload.com/pl/%s/425x350/swf" />
-          <embed src="http://%ssevenload.com/pl/%s/425x350/swf" type="application/x-shockwave-flash" width="425" height="350" FlashVars="slxml=%ssevenload.com" wmode="transparent" ></embed>
+        <object type="application/x-shockwave-flash" data="http://#{lang}.sevenload.com/pl/#{video_id}/#{options[:width]}x#{options[:height]}/swf" width="#{options[:width]}" height="#{options[:height]}">
+          <param name="allowFullscreen" value="true" />
+          <param name="allowScriptAccess" value="always" />
+          <param name="movie" value="http://#{lang}.sevenload.com/pl/#{video_id}/#{options[:width]}x#{options[:height]}/swf" />
         </object>
       }
-      # % [m[1], m[1], m[3], m[1], m[3], m[1]]
     end
-        
+    
+    def title
+      page.at("#itemTitle").inner_html
+    end
+    
     def self.match(url)
-      url.match(/([\w|\.]+)sevenload\.\S{2,}\/(videos|videolar|filmy)?\/(\w+)[\/]?([\S]+)/xi)
+      url.match(/(\w+)\.sevenload\.\S{2,}\/(videos|videolar|filmy|video)?\/(\w+)[\/]?([\S]+)/xi) || url.match(/(\w+)\.sevenload\.\S{2,}\/(shows|sendungen|yayinlar|emissions|trasmissione|audycje)?\/(\S+)\/(episodes|folgen|bolumler|puntate)?\/(\w+)[\/]?([\S]+)/xi)
     end
     
     private
     
+    def page
+      @page ||= Hpricot(open(@url))
+    end
+    
     def get_info
       match = self.class.match(@url)
       @lang = match[1]
-      
+      # the video id is always the second last match
+      @video_id = match[-2]
     end
     
   end
