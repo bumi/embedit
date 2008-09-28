b595d4e9c565f4e80ebc22afa54eed9ea49ebc7a
Michael Bumann <michael@railslove.com>
2008-09-28 16:25:24 +0200

    request webpage and extract title only when needed (improve speed)

--- lib/embedit/providers/youtube.rb
+++ lib/embedit/providers/youtube.rb
@@ -7,38 +7,34 @@ module Embedit
     def initialize(url)
       @format = 'video'
       @url = url
-      get_info
     end
     
     def html(options = {})
-      options.reverse_merge({:width=>425,:height=>350})
+      options.reverse_merge!({:width=>425,:height=>350})
       %{
         <object width="#{options[:width]}" height="#{options[:height]}">
-          <param name="movie" value="http://www.youtube.com/v/#{@video_id}"></param>
+          <param name="movie" value="http://www.youtube.com/v/#{video_id}"></param>
           <param name="wmode" value="transparent"></param>
-          <embed src="http://www.youtube.com/v/#{@video_id}" 
+          <embed src="http://www.youtube.com/v/#{video_id}" 
             type="application/x-shockwave-flash" wmode="transparent" 
             width="#{options[:width]}" height="#{options[:height]}">
           </embed>
         </object>
       }
     end
+    
+    def video_id
+      @video_id ||= url[/v=([\w\d]+)/,1]
+    end
+    
+    def title
+      return @title if @title
+      data = REXML::Document.new(open("http://gdata.youtube.com/feeds/videos/#{video_id}"))
+      @title = REXML::XPath.first(data, "//title").text
+    end
         
     def self.match(url)
       url.match(/youtube\.com\/watch\?v=(\S+)/xi)
-    end
-    
-    private
-    
-    def get_info
-      video_id = extract_id(@url)
-      data = REXML::Document.new(open("http://gdata.youtube.com/feeds/videos/#{video_id}"))
-      @title = REXML::XPath.first(data, "//title").text
-      @html = video_id
-    end
-    
-    def extract_id(url)
-      url.scan(/v=([\w\d]+)/)
     end
   
   end
