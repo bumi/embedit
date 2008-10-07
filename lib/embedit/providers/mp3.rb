605a85d8022e1b27d44e20748f6652c84e9e635b
Michael Bumann <michael@railslove.com>
2008-10-07 19:43:40 +0200

    added support for mp3

--- lib/embedit/providers/mp3.rb
+++ lib/embedit/providers/mp3.rb
@@ -9,9 +9,18 @@ module Embedit
     end
     
     def html(options = {})
-      attributes = options.collect {|k,v| "#{k}=\"#{v}\" "}.join(" ")
+      options[:width] ||= 275
+      options[:height] ||= 50
+      options[:player] ||= "/public/mp3.swf"
+      options[:preview] ||= ""
+      attributes = options.collect {|k,v| "#{k}=\"#{v}\" "}.join("&")
       %{
-        <a href="#{@url}"><img src="#{@url}" #{attributes}/></a>
+        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="#{options[:width]}" height="#{options[:heigth]}" align="middle">
+        <param name="allowScriptAccess" value="sameDomain" />
+        <param name="movie" value="#{options[:player]}?mp3url=#{@url}&#{attributes}" />
+        <param name="quality" value="high" />
+        <param name="bgcolor" value="#222222" />
+        <embed src="#{options[:player]}?mp3url=#{@url}&#{attributes}" quality="high" width="#{options[:width]}" height="#{options[:height]}" name="mp3" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
       } 
     end
     
